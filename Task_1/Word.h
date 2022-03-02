d3c31f8a6413d3863c5f240930a0503387219b48
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2022-03-02 15:19:40 +0700

    Implement dictionary reading

---
+++ Task_1/Word.h
@@ -0,0 +1,17 @@
+//
+// Created by Konstantin Moskalenko on 25.02.2022.
+//
+
+#ifndef TASK_1_WORD_H
+#define TASK_1_WORD_H
+
+#include <string>
+#include <vector>
+
+struct Word {
+    std::string text;
+    std::vector<std::string> grammemes;
+};
+
+
+#endif //TASK_1_WORD_H
--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Word.h"
+
+std::vector<Word> readDictionary(const std::string &filepath) {
+    std::ifstream dictFile(filepath);
+    std::string line;
+
+    std::vector<Word> words;
+
+    while (std::getline(dictFile, line)) {
+        if (line.empty() || std::isdigit(line[0])) continue;
+        Word word;
+
+        std::string::iterator it = line.begin();
+        while (!std::isspace(*it)) {
+            word.text += *it;
+            it++;
+        }
+
+        it++; // Skipping whitespace
+
+        while (it != line.end()) {
+            std::string grammeme;
+            while (*it != ',' && it != line.end()) {
+                grammeme += *it;
+                it++;
+            }
+
+            if (*it == ',') it++;
+            word.grammemes.push_back(grammeme);
+        }
+
+        words.push_back(word);
+    }
+
+    return words;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const auto words = readDictionary("../resources/dict.opcorpora.txt");
+
     return 0;
 }
