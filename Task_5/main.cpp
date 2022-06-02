06045da6c5631e7d5cfcaa8d59960f0f0916610a
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2022-06-02 23:37:57 +0700

    Use thesaurus for calculating ranking score

--- Task_5/main.cpp
+++ Task_5/main.cpp
@@ -6,6 +6,7 @@
 
 #include "../Parser/Parser.h"
 #include "Term.h"
+#include "Thesaurus.h"
 
 namespace BM25 {
     constexpr double k1 = 2.0;
@@ -14,6 +15,9 @@ namespace BM25 {
 
 Parser::Dictionary dictionary;
 std::unordered_map<std::string, Term *> termDictionary;
+
+std::unordered_map<std::string, int> documentSize;
+unsigned long averageDocumentSize = 0;
 
 std::vector<std::string> normalizeRequest(const std::wstring &request) {
     std::vector<std::string> result;
@@ -32,14 +36,40 @@ std::vector<std::string> normalizeRequest(const std::wstring &request) {
     return result;
 }
 
+std::unordered_map<std::string, double> getDocumentsScore(const std::string &word) {
+    std::unordered_map<std::string, double> documentScore;
+    if (termDictionary.find(word) == termDictionary.end()) {
+        return documentScore;
+    }
+
+    const auto entries = termDictionary.at(word)->entries;
+    const auto totalDocs = (double) documentSize.size();
+    const auto docsWithWord = (double) entries.size();
+    const double idf = log((totalDocs - docsWithWord + 0.5) / (docsWithWord + 0.5));
+
+    for (const auto &entry: entries) {
+        const auto docSize = documentSize.at(entry.first);
+        const auto f = (double) entry.second.positions.size() / docSize;
+
+        if (documentScore.find(entry.first) == documentScore.end()) {
+            documentScore.emplace(entry.first, 0.0);
+        }
+
+        documentScore.at(entry.first) += idf * (f * (BM25::k1 + 1)) / (
+                f + BM25::k1 * (1 - BM25::b + BM25::b * (
+                        docSize / (double) averageDocumentSize
+                ))
+        );
+    }
+
+    return documentScore;
+}
+
 int main() {
     std::setlocale(LC_ALL, "ru_RU.UTF-8");
 
     dictionary = Parser::readDictionary(DICTIONARY_PATH, 6000000);
     const auto iterator = std::filesystem::directory_iterator(NEWS_PATH);
-
-    std::unordered_map<std::string, int> documentSize;
-    unsigned long averageDocumentSize = 0;
 
     for (const auto &entry: iterator) {
         std::wifstream wif(entry.path());
@@ -75,29 +105,30 @@ int main() {
 
     const auto request = L"";
     const auto words = normalizeRequest(request);
+
     std::unordered_map<std::string, double> documentScore;
+    std::unordered_map<std::string, const Descriptor *> synsets;
+
+    for (const auto &descriptor: thesaurus) {
+        for (const auto &synonym: descriptor.synset) {
+            synsets.emplace(synonym, &descriptor);
+        }
+    }
 
     for (const auto &word: words) {
-        if (termDictionary.find(word) == termDictionary.end()) continue;
+        auto scores = getDocumentsScore(word);
+        if (synsets.find(word) != synsets.end()) {
+            const auto merge = [&scores](const decltype(scores) &pairs, double coeff) {
+                for (const auto &pair: pairs) scores[pair.first] = std::max(scores[pair.first], coeff * pair.second);
+            };
 
-        const auto entries = termDictionary.at(word)->entries;
-        const auto totalDocs = (double) documentSize.size();
-        const auto docsWithWord = (double) entries.size();
-        const double idf = log((totalDocs - docsWithWord + 0.5) / (docsWithWord + 0.5));
+            for (const auto &syn: synsets[word]->synset) merge(getDocumentsScore(syn), 0.5);
+            for (const auto &hyper: synsets[word]->hyperonym) merge(getDocumentsScore(hyper), 0.25);
+            for (const auto &hypo: synsets[word]->hyponym) merge(getDocumentsScore(hypo), 0.25);
+        }
 
-        for (const auto &entry: entries) {
-            const auto docSize = documentSize.at(entry.first);
-            const auto f = (double) entry.second.positions.size() / docSize;
-
-            if (documentScore.find(entry.first) == documentScore.end()) {
-                documentScore.emplace(entry.first, 0.0);
-            }
-
-            documentScore.at(entry.first) += idf * (f * (BM25::k1 + 1)) / (
-                    f + BM25::k1 * (1 - BM25::b + BM25::b * (
-                            docSize / (double) averageDocumentSize
-                    ))
-            );
+        for (const auto &score: scores) {
+            documentScore[score.first] += score.second;
         }
     }
 
